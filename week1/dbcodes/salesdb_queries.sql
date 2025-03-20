684020a3d25dae9fbaba35279fb5bef3ac2b425b
Tristan bryan <tristan@kali.tristan>
2025-03-20 22:07:30 +0300

    modified sql prompts

--- week1/dbcodes/salesdb_queries.sql
+++ week1/dbcodes/salesdb_queries.sql
@@ -5,7 +5,8 @@ DROP TABLE IF EXISTS SalesDB.Products;
 CREATE TABLE SalesDB.Products (
     ProductID INT PRIMARY KEY AUTO_INCREMENT,
     ProductName VARCHAR(255) NOT NULL,
-    Price DECIMAL(10,2) NOT NULL
+    Price DECIMAL(10,2) NOT NULL,
+    DateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 /*  inserting values to table*/
@@ -14,14 +15,15 @@ INSERT INTO SalesDB.Products (ProductName, Price) VALUES
 ( 'Laptop', '500.00'),
 ( 'Tablet', '200.00'),
 ('Smartphone' , '300.00'),
-('Television', '700.00');
+('Television', '700.00'),
+('Subwoofer', '900.00');
 
-
-/*  selecting where price is greater than 500*/
-SELECT * FROM SalesDB.Products WHERE PRICE > 500;
+/*  selecting where price is greater than 500 (used wildcards for flexible searching)*/
+SELECT * FROM SalesDB.Products WHERE PRICE > 500 AND ProductName = 'Laptop';
 
 /*  selecting to order by price*/
 SELECT * FROM SalesDB.Products ORDER BY Price DESC;
+SELECT * FROM SalesDB.Products ORDER BY Price ASC;
 
 /*  selecting where product name has this spelling {l}*/
 SELECT * FROM SalesDB.Products WHERE ProductName LIKE 'L%';
@@ -32,8 +34,5 @@ UPDATE Products
 SET Category = 'Stationery'
 WHERE Category = 'Stationary';
 
- /*  Truncating a table*/
- TRUNCATE TABLE SalesDB.Products;
-
-
-
+/*  Truncating a table*/
+TRUNCATE TABLE SalesDB.Products;
