6fd37c428954e0a1d14ed60f33c75c99eb9a5ce7
Tristan bryan <tristan@kali.tristan>
2025-05-13 09:00:31 +0300

    update libraries file

--- week5/ex_lib.py
+++ week5/ex_lib.py
@@ -1,5 +1,9 @@
 import requests
 
+# This script demonstrates how to use the requests library to make a GET request to a public API
+# and print the response in JSON format.
+# Import the requests library
+
 response = requests.get('https://api.github.com/events')
 print(response.json())
 
